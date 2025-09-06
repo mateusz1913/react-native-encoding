7b6c9fec08e4082735aa30e4748d0fadc6b315a2
Mateusz Mędrek <mateusz.medrek@callstack.com>
2025-09-06 18:27:13 +0200

    chore: lint codebase

--- cpp/ReactNativeEncodingModule.h
+++ cpp/ReactNativeEncodingModule.h
@@ -15,17 +15,17 @@ public:
     std::string convertFromArrayBufferToHex(jsi::Runtime &rt, jsi::Object arrayBuffer);
     std::string convertFromArrayBufferToUTF8(jsi::Runtime &rt, jsi::Object arrayBuffer);
 
-    jsi::Object convertFromBase64ToArrayBuffer(jsi::Runtime &rt, const std::string& base64Str);
-    std::string convertFromBase64ToHex(jsi::Runtime &rt, const std::string& base64Str);
-    std::string convertFromBase64ToUTF8(jsi::Runtime &rt, const std::string& base64Str);
+    jsi::Object convertFromBase64ToArrayBuffer(jsi::Runtime &rt, const std::string &base64Str);
+    std::string convertFromBase64ToHex(jsi::Runtime &rt, const std::string &base64Str);
+    std::string convertFromBase64ToUTF8(jsi::Runtime &rt, const std::string &base64Str);
 
-    jsi::Object convertFromHexToArrayBuffer(jsi::Runtime &rt, const std::string& hexStr);
-    std::string convertFromHexToBase64(jsi::Runtime &rt, const std::string& hexStr);
-    std::string convertFromHexToUTF8(jsi::Runtime &rt, const std::string& hexStr);
+    jsi::Object convertFromHexToArrayBuffer(jsi::Runtime &rt, const std::string &hexStr);
+    std::string convertFromHexToBase64(jsi::Runtime &rt, const std::string &hexStr);
+    std::string convertFromHexToUTF8(jsi::Runtime &rt, const std::string &hexStr);
 
-    jsi::Object convertFromUTF8ToArrayBuffer(jsi::Runtime &rt, const std::string& utf8Str);
-    std::string convertFromUTF8ToBase64(jsi::Runtime &rt, const std::string& utf8Str);
-    std::string convertFromUTF8ToHex(jsi::Runtime &rt, const std::string& utf8Str);
+    jsi::Object convertFromUTF8ToArrayBuffer(jsi::Runtime &rt, const std::string &utf8Str);
+    std::string convertFromUTF8ToBase64(jsi::Runtime &rt, const std::string &utf8Str);
+    std::string convertFromUTF8ToHex(jsi::Runtime &rt, const std::string &utf8Str);
 };
 
 } // namespace facebook::react
